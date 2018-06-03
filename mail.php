71216b58015540bfc0ab7372ab85929bb388d1db
Sebastien DIAZ <sebastien.diaz@misys.com>
2018-06-03 19:37:47 +0200

    Add use case 1

--- mail.php
+++ mail.php
@@ -13,7 +13,7 @@ if($_POST["message"]){
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
     if($result['success']){
-        //var_dump($result);
+        var_dump($result);
 
         $to      = 'sebastien.diaz@gmail.com';
         $subject = '[Buisson Diaz Conseil] ';
