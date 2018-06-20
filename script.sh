28c4ed6a9c83a09c43398006b76bbb47cd3934e3
Sebastien DIAZ <sebastien.diaz@misys.com>
2018-06-20 21:33:43 +0200

    add more

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ quote USER $USER
 quote PASS $PASSWD
 cd www
 bin
-mput index.php mail.php sitemap.xml robots.txt
+mput mail.php sitemap.xml robots.txt siteweb/index.html siteweb/approach.html siteweb/contact.html  siteweb/data.html siteweb/expertise.html siteweb/mentionslegales.html siteweb/privacy.html siteweb/software.html siteweb/sport.html siteweb/terms.html
 quit
 END_SCRIPT
 exit 0
