dc2f69358c2b1f4983118487111c693d639bc379
Sebastien DIAZ <sebastien.diaz@misys.com>
2018-06-20 22:36:29 +0200

    add back

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ quote USER $USER
 quote PASS $PASSWD
 cd www
 bin
-mput mail.php sitemap.xml robots.txt siteweb/index.html siteweb/approach.html siteweb/contact.html  siteweb/data.html siteweb/expertise.html siteweb/mentionslegales.html siteweb/privacy.html siteweb/software.html siteweb/sport.html siteweb/terms.html
+mput mail.php sitemap.xml robots.txt siteweb/index.html siteweb/approach.html siteweb/contact.html  siteweb/data.html siteweb/expertise.html siteweb/mentionslegales.html siteweb/privacy.html siteweb/software.html siteweb/sport.html siteweb/terms.html siteweb/cgv_service.pdf
 quit
 END_SCRIPT
 exit 0
